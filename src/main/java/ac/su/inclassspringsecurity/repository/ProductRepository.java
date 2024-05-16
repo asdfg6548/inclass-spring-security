92d0af94e22ec40580cccc9cfcac4faed26b1a70
asdfg6548 <asdfg6548@syuin.ac.kr>
2024-05-16 14:28:47 +0900

    test 추가

--- src/main/java/ac/su/inclassspringsecurity/repository/ProductRepository.java
+++ src/main/java/ac/su/inclassspringsecurity/repository/ProductRepository.java
@@ -1,2 +1,22 @@
-package ac.su.inclassspringsecurity.repository;public interface ProductRepository {
+package ac.su.inclassspringsecurity.repository;
+
+
+import ac.su.inclassspringsecurity.constant.ProductStatusEnum;
+import ac.su.inclassspringsecurity.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>, QuerydslPredicateExecutor<Product> {
+    List<Product> findByName(String name);
+    List<Product> findByStatus(ProductStatusEnum status);
+
+
+    @Query("SELECT p FROM Product p WHERE p.status in :statusList")
+    List<Product> findByStatusList(List<ProductStatusEnum> preparing);
 }
